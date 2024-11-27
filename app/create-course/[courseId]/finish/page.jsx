60be9f1c4ab87528e5c39a165563959845fcb21e
Nag <nalamnagendra1611@gmail.com>
2024-11-27 18:13:33 +0530

    Initial commit

--- app/create-course/[courseId]/finish/page.jsx
+++ app/create-course/[courseId]/finish/page.jsx
@@ -6,15 +6,17 @@ import { and, eq } from "drizzle-orm";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import CourseBasicInfo from "../_components/CourseBasicInfo";
-// import { HiOutlineClipboardDocumentCheck } from "react-icons/hi2";
+import { HiOutlineClipboardDocumentCheck } from "react-icons/hi2"; // Corrected import
 
 function FinishScreen({ params }) {
   const { user } = useUser();
   const [course, setCourse] = useState([]);
   const router = useRouter();
+
   useEffect(() => {
     params && GetCourse();
   }, [params, user]);
+
   const GetCourse = async () => {
     const result = await db
       .select()
@@ -27,15 +29,24 @@ function FinishScreen({ params }) {
       );
     setCourse(result[0]);
   };
+
   return (
     <div className="px-10 md:px-20 lg:px-44 my-7">
       <h2 className="text-center font-bold text-2xl my-3 text-primary">
         Congrats!🥳 Click on Dashboard
       </h2>
       <CourseBasicInfo course={course} refreshData={() => console.log()} />
-        <h2 className="mt-3">Course URL:</h2>
+      <h2 className="mt-3">Course URL:</h2>
       <h2 className="text-center text-gray-400 border p-2 rounded flex gap-6 items-center">
-        {process.env.NEXT_PUBLIC_HOST_NAME}/course/{course?.courseId} <HiOutlineClipboardDocumentCheck className="h-5 w-5 cursor-pointer" onClick={async()=>await navigator.clipboard.writeText(process.env.NEXT_PUBLIC_HOST_NAME+"/course/"+course?.courseId)}/>
+        {process.env.NEXT_PUBLIC_HOST_NAME}/course/{course?.courseId}{" "}
+        <HiOutlineClipboardDocumentCheck
+          className="h-5 w-5 cursor-pointer"
+          onClick={async () =>
+            await navigator.clipboard.writeText(
+              process.env.NEXT_PUBLIC_HOST_NAME + "/course/" + course?.courseId
+            )
+          }
+        />
       </h2>
     </div>
   );
