3bd3a8a8c757e0ad0abbaa3f82a8d58f9c4f5bb9
Nag <nalamnagendra1611@gmail.com>
2024-12-01 00:04:44 +0530

    Initial commit

--- app/dashboard/logout/page.jsx
+++ app/dashboard/logout/page.jsx
@@ -8,7 +8,7 @@ const LogoutPage = () => {
         flexDirection: 'column',
         justifyContent: 'flex-end',
         alignItems: 'center',
-        height: '85vh',
+        height: '90vh',
         backgroundImage: 'url("/logout.jpeg")', // Assuming logout.jpg is in the public folder
         backgroundSize: 'cover',
         backgroundPosition: 'center',
