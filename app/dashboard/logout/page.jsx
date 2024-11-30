fdb4338260c5eb1c13893187e7dc158a9c722e0f
Nag <nalamnagendra1611@gmail.com>
2024-11-30 23:53:33 +0530

    Initial commit

--- app/dashboard/logout/page.jsx
+++ app/dashboard/logout/page.jsx
@@ -1,11 +1,33 @@
-import {SignOutButton}from'@clerk/nextjs';
+import {SignOutButton} from '@clerk/nextjs';
 
-const LogoutPage=()=>{
-  return(
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
+const LogoutPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundImage: 'url("/logout.jpeg")', // Assuming logout.jpg is in the public folder
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <SignOutButton>
-        <button style={{padding:'10px 20px',fontSize:'16px',backgroundColor:'#0070f3',color:'#fff',border:'none',borderRadius:'5px',cursor:'pointer'}}>
-            Log out
+        <button
+          style={{
+            padding: '10px 20px',
+            fontSize: '16px',
+            backgroundColor: '#0070f3',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            marginBottom: '20px', // Add spacing from the bottom
+          }}
+        >
+          Log out
         </button>
       </SignOutButton>
     </div>
